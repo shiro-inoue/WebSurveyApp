649a5c9afeff7532d137cf9a1a5f1a86b9dd0777
shiro-inoue <shiro_inoue@sobal.co.jp>
2020-10-13 09:33:37 +0900

    jsファイル実装 現在日時表示

--- aggregate.js
+++ aggregate.js
@@ -3,14 +3,35 @@ function dispResponseRate() {
 }
 
 function dispDayTime() {
-    document.getElementById('dateTime').innerHTML = "<p>2020/10/12 16:25現在</p>";
+    let now = new Date();
+    let year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    month = editDataFormat(month);
+    let date = now.getDate();
+    date = editDataFormat(date);
+    let hour = now.getHours();
+    hour = editDataFormat(hour);
+    let min = now.getMinutes();
+    min = editDataFormat(min);
+
+    document.getElementById('dateTime').innerHTML = year + "/" + month + "/" + date + " " + hour + ":" + min + "現在";
+
 }
 
 function dispAnswer() {
     let qId = new Array("q1", "q2", "q3", "q4", "q5");
-
     qId.forEach((id, index) => {
         console.log("getAnswer(" + `${id}` + ")");
         // getAnswer(`${id}`);
     });
+}
+
+function editDataFormat(data) {
+    console.log("In  data = " + data);
+    // 文字列で扱えたらlengthが使える？
+    if (data < "10") {
+        data = "0" + data;
+    }
+    console.log("Out data = " + data);
+    return data;
 }

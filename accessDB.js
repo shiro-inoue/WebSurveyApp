f0a4767a8589ba9f5c932073cf60fc3d3d6a1aaa
keisuke_nakamura <keisuke_nakamura@sobal.co.jp>
2020-10-14 09:53:22 +0900

    getAnsweredRatedata追加、getAnswerにidの引数追加。

--- accessDB.js
+++ accessDB.js
@@ -89,57 +89,59 @@ let employeeTest3 = {
         }
     ]
 }
-let answerTest = {
-    "total": 200,
-    "response": 50,
-    "question": [
-        {
-            "title": "あなたは無事ですか？",
-            "questiontype": QUESTIONTYPE.radio,
-            "sub": [
-                { "title": "怪我などは無い", "select": 21 },
-                { "title": "怪我をしている", "select": 29 },
-            ]
-        },
-        {
-            "title": "家族は無事ですか？",
-            "questiontype": QUESTIONTYPE.radio,
-            "sub": [
-                { "title": "全員無事", "select": 42 },
-                { "title": "連絡が取れない家族がいる", "select": 8 },
-            ]
-        },
-        {
-            "title": "明日の出勤は可能ですか？",
-            "questiontype": QUESTIONTYPE.radio,
-            "sub": [
-                { "title": "出勤可能", "select": 28 },
-                { "title": "出勤はできない", "select": 2 },
-                { "title": "わからない", "select": 20 },
-            ]
-        },
-        {
-            "title": "心配事があれば、選択してください（複数選択可）",
-            "questiontype": QUESTIONTYPE.check,
-            "sub": [
-                { "title": "家が倒壊した", "select": 4 },
-                { "title": "ペットが見つからない", "select": 2 },
-                { "title": "避難所にいる", "select": 3 },
-                { "title": "その他", "select": 5 },
-            ]
-        },
-        {
-            "title": "会社に伝えたいこと、不安などがあれば、ご記入ください。",
-            "questiontype": QUESTIONTYPE.text,
-            "sub": [
-                { "text": "test1 anxiety1" },
-                { "text": "test30 anxiety2" },
-            ]
-        }
+let answerTest1 = {
+    "title": "あなたは無事ですか？",
+    "questiontype": QUESTIONTYPE.radio,
+    "sub": [
+        { "title": "怪我などは無い", "select": 21 },
+        { "title": "怪我をしている", "select": 29 },
     ]
 }
+let answerTest2 = {
+    "title": "家族は無事ですか？",
+    "questiontype": QUESTIONTYPE.radio,
+    "sub": [
+        { "title": "全員無事", "select": 42 },
+        { "title": "連絡が取れない家族がいる", "select": 8 },
+    ]
+}
+let answerTest3 = {
+    "title": "明日の出勤は可能ですか？",
+    "questiontype": QUESTIONTYPE.radio,
+    "sub": [
+        { "title": "出勤可能", "select": 28 },
+        { "title": "出勤はできない", "select": 2 },
+        { "title": "わからない", "select": 20 },
+    ]
+}
+let answerTest4 = {
+    "title": "心配事があれば、選択してください（複数選択可）",
+    "questiontype": QUESTIONTYPE.check,
+    "sub": [
+        { "title": "家が倒壊した", "select": 4 },
+        { "title": "ペットが見つからない", "select": 2 },
+        { "title": "避難所にいる", "select": 3 },
+        { "title": "その他", "select": 5 },
+    ]
+}
+let answerTest5 = {
+    "title": "会社に伝えたいこと、不安などがあれば、ご記入ください。",
+    "questiontype": QUESTIONTYPE.text,
+    "sub": [
+        { "text": "test1 anxiety1" },
+        { "text": "test30 anxiety2" },
+    ]
+}
+let answerTest6 = {
+    "title": "",
+}
 
-function getEmployeeAnswer(json){
+let RatedataTest = {
+    "total": 200,
+    "response": 50
+}
+
+function getEmployeeAnswer(json) {
     let jsonParse = JSON.parse(json);
 
     if (jsonParse.id == "000001") {
@@ -153,10 +155,29 @@ function getEmployeeAnswer(json){
     }
 }
 
-function setEmployeeAnswer(json){
+function setEmployeeAnswer(json) {
     return ;
 }
 
-function getAnswer(){
-    return JSON.stringify(answerTest);
+function getAnsweredRatedata() {
+    return JSON.stringify(RatedataTest);
+}
+
+function getAnswer(json){
+    let jsonParse = JSON.parse(json);
+
+    switch( jsonParse.id ) {
+    case 1:
+        return JSON.stringify(answerTest1);
+    case 2:
+        return JSON.stringify(answerTest2);
+    case 3:
+        return JSON.stringify(answerTest3);
+    case 4:
+        return JSON.stringify(answerTest4);
+    case 5:
+        return JSON.stringify(answerTest5);
+    default:
+        return JSON.stringify(answerTest6);
+    }
 }
